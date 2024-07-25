a02ec859968184d187d3c6095354b147bf692703
WT-MM <wesley.maa@gmail.com>
2024-07-24 22:41:18 -0700

    visualize from tensorflow datasets

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -27,8 +27,9 @@ else:
 
 # create TF dataset
 dataset_name = args.dataset_name
+
+# dataset should (by default) be located in ~/tensorflow_datasets/
 print(f"Visualizing data from dataset: {dataset_name}")
-module = importlib.import_module(dataset_name)
 ds = tfds.load(dataset_name, split='train')
 ds = ds.shuffle(100)
 
